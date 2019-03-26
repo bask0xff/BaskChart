e433e0bcfe277ff7e641e8f3b2d7bcbaabce24e4
s.baskov <s.baskov@spherelab.ru>
2019-03-26 16:58:39 +0300

    pass touchezs to ChartGLView

--- app/src/main/java/ru19july/tgchart/view/opengl/ChartGLView.java
+++ app/src/main/java/ru19july/tgchart/view/opengl/ChartGLView.java
@@ -3,18 +3,29 @@ package ru19july.tgchart.view.opengl;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 import ru19july.tgchart.data.ChartData;
 import ru19july.tgchart.view.IChartView;
 import ru19july.tgchart.view.theme.IChartTheme;
 
-public class ChartGLView extends GLSurfaceView implements IChartView {
+public class ChartGLView extends GLSurfaceView implements IChartView, View.OnTouchListener  {
+    private MyRenderer mRenderer;
+
     public ChartGLView(Context context) {
         super(context);
+        init();
     }
 
     public ChartGLView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        setOnTouchListener(this);
     }
 
     @Override
@@ -43,7 +54,13 @@ public class ChartGLView extends GLSurfaceView implements IChartView {
     }
 
     @Override
-    public void setRenderer(MyRenderer mRenderer) {
-        super.setRenderer(mRenderer);
+    public void setRenderer(MyRenderer renderer) {
+        super.setRenderer(renderer);
+        mRenderer = renderer;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mRenderer.onTouchEvent(event);
     }
 }
